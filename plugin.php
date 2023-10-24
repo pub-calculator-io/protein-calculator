5ef20384694bee2f7f4d514f2b661b22b07f78be
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:05:53 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_protein_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Protein Calculator by Calculator.iO";
 
 function display_ci_protein_calculator(){
